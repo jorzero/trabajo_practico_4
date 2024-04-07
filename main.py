351e7de80b3704ad4fe89e512c79e6aaa3a68044
Lopez Venegas <jlopezvn@amazon.com>
2024-04-06 23:20:18 -0600

    Agregar primera funcion

---
+++ main.py
@@ -0,0 +1,7 @@
+# Función para pedir el nombre al usuario y saludar
+def saludo_personalizado():
+    nombre = input("Por favor, ingresa tu nombre: ")  # Captura la entrada del usuario
+    print(f"Hola, {nombre}! Bienvenido/a.")  # Muestra el mensaje de saludo personalizado
+
+# Llamar a la función
+saludo_personalizado()
